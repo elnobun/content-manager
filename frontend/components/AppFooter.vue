71b5473e59f218080a6189c4ee095dbd1716552d
Ellis Enobun <elnobun@gmail.com>
2019-08-02 16:12:43 +0100

    Login and Register component setup

---
+++ frontend/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-footer app>
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: "AppFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,55 +1,64 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+    <v-app>
+        <app-header></app-header>
+        <v-content>
+            <nuxt/>
+            <v-btn
+                v-scroll="onScroll"
+                v-show="fab"
+                fab
+                dark
+                fixed
+                bottom
+                right
+                color="primary"
+                @click="toTop"
+            >
+                <v-icon>keyboard_arrow_up</v-icon>
+            </v-btn>
+        </v-content>
+        <app-footer></app-footer>
+    </v-app>
 </template>
 
+<script>
+import AppHeader from "../components/AppHeader";
+import AppFooter from "../components/AppFooter";
+
+export default {
+    components: {AppFooter, AppHeader},
+
+    data: () => ({
+        fab: false
+    }),
+
+    methods: {
+        onScroll(e) {
+            if (typeof window === 'undefined') return;
+            const top = window.pageYOffset || e.target.scrollTop || 0;
+            this.fab = top > 20
+        },
+        async toTop() {
+            await this.$vuetify.goTo(0)
+        }
+    },
+}
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+    html {
+        word-spacing: 1px;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        box-sizing: border-box;
+    }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+    *,
+    *:before,
+    *:after {
+        box-sizing: border-box;
+        margin: 0;
+    }
 </style>
